778f80ac144a1687fa1efe5291eb69e0a1329870
Thomas Berlok <tberlok@gmail.com>
2022-03-21 22:07:24 +0100

    Cleaned up duplicate code

--- tests/test_compressible_wave_standing.py
+++ tests/test_compressible_wave_standing.py
@@ -1,6 +1,48 @@
+import numpy as np
 
-def test_isothermal_compressible_wave_standing(show=False, ii=0):
-    import numpy as np
+
+def compare(show, a, an_sol, sci_sol):
+    """
+    Do the testing and plot if requested
+    """
+    np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).real,
+                               sci_sol.delta_rhoc_over_rhoc(a).real, atol=1e-5)
+    np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).imag,
+                               sci_sol.delta_rhoc_over_rhoc(a).imag, atol=1e-5)
+    np.testing.assert_allclose(an_sol.delta_u_over_vs(a).real,
+                               sci_sol.delta_u_over_vs(a).real, atol=1e-5)
+    np.testing.assert_allclose(an_sol.delta_u_over_vs(a).imag,
+                               sci_sol.delta_u_over_vs(a).imag, atol=1e-5)
+
+    if show:
+        import matplotlib.pyplot as plt
+        plt.figure(1)
+        plt.clf()
+        fig, axes = plt.subplots(num=1, nrows=2, ncols=2, sharex=True)
+
+        axes[0, 0].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).real, label='Analytic')
+        axes[0, 1].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).imag)
+        axes[1, 0].semilogx(a, an_sol.delta_u_over_vs(a).real)
+        axes[1, 1].semilogx(a, an_sol.delta_u_over_vs(a).imag)
+        axes[0, 0].set_title(r'$\mathrm{Re}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
+        axes[0, 1].set_title(r'$\mathrm{Im}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
+        axes[1, 0].set_title(r'$\mathrm{Re}(\delta u/\mathcal{V}_\mathrm{A})$')
+        axes[1, 1].set_title(r'$\mathrm{Im}(\delta u/\mathcal{V}_\mathrm{A})$')
+
+        axes[0, 0].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).real, '--', label='Scipy')
+        axes[0, 1].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).imag, '--')
+        axes[1, 0].semilogx(a, sci_sol.delta_u_over_vs(a).real, '--')
+        axes[1, 1].semilogx(a, sci_sol.delta_u_over_vs(a).imag, '--')
+        axes[0, 0].legend(frameon=False)
+        nums = '{:1.2f},{:1.2f},{:1.2f}'.format(an_sol.OmegaS/np.pi, an_sol.OmegaA/np.pi, an_sol.OmegaG/np.pi)
+        title = r',\, (\Omega_\mathrm{s},\,\Omega_\mathrm{A},\,\Omega_\mathrm{g})/\pi = ' + nums + '$'
+        title = r'$\gamma=' + '{:1.2f}'.format(an_sol.gamma) + title
+        fig.suptitle(title)
+
+        plt.show()
+
+
+def test_isothermal_compressible_wave_standing(show=False):
     from comoving_mhd_waves import AnalyticComovingMagnetosonicWave
     from comoving_mhd_waves import ScipyComovingMagnetosonicWave
 
@@ -13,53 +55,13 @@ def test_isothermal_compressible_wave_standing(show=False, ii=0):
 
     a = np.logspace(np.log10(ai), 0, 300)
 
-    # Isothermal test
     Vs = 1.0
     gamma = 1
     for Va in [0, 1/2]:
         for Vg in [0, 1/4]:
             an_sol = AnalyticComovingMagnetosonicWave(k, H0, Vs, Va, Vg, gamma, ai, A_u, A_rho)
             sci_sol = ScipyComovingMagnetosonicWave(k, H0, Vs, Va, Vg, gamma, ai, A_u, A_rho)
-
-            if show:
-                import matplotlib.pyplot as plt
-                plt.figure(ii+1)
-                plt.clf()
-                fig, axes = plt.subplots(num=ii+1, nrows=2, ncols=2, sharex=True)
-
-                axes[0, 0].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).real, label='Analytic')
-                axes[0, 1].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).imag)
-                axes[1, 0].semilogx(a, an_sol.delta_u_over_vs(a).real)
-                axes[1, 1].semilogx(a, an_sol.delta_u_over_vs(a).imag)
-                axes[0, 0].set_title(r'$\mathrm{Re}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
-                axes[0, 1].set_title(r'$\mathrm{Im}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
-                axes[1, 0].set_title(r'$\mathrm{Re}(\delta u/\mathcal{V}_\mathrm{A})$')
-                axes[1, 1].set_title(r'$\mathrm{Im}(\delta u/\mathcal{V}_\mathrm{A})$')
-
-                axes[0, 0].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).real, '--', label='Scipy')
-                axes[0, 1].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).imag, '--')
-                axes[1, 0].semilogx(a, sci_sol.delta_u_over_vs(a).real, '--')
-                axes[1, 1].semilogx(a, sci_sol.delta_u_over_vs(a).imag, '--')
-                axes[0, 0].legend(frameon=False)
-                nums = '{:1.2f},{:1.2f},{:1.2f}'.format(an_sol.OmegaS/np.pi, an_sol.OmegaA/np.pi, an_sol.OmegaG/np.pi)
-                title = r'$\gamma=1,\, (\Omega_\mathrm{s},\,\Omega_\mathrm{A},\,\Omega_\mathrm{g})/\pi = ' + nums + '$'
-                fig.suptitle(title)
-
-                ii += 1
-
-                # Test that everything works
-                np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).real,
-                                           sci_sol.delta_rhoc_over_rhoc(a).real, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).imag,
-                                           sci_sol.delta_rhoc_over_rhoc(a).imag, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_u_over_vs(a).real,
-                                           sci_sol.delta_u_over_vs(a).real, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_u_over_vs(a).imag,
-                                           sci_sol.delta_u_over_vs(a).imag, atol=1e-4)
-    if show:
-        plt.show()
-
-    return ii
+            compare(show, a, an_sol, sci_sol)
 
 
 def test_gamma_four_thirds_compressible_wave_standing(show=False, ii=0):
@@ -82,85 +84,15 @@ def test_gamma_four_thirds_compressible_wave_standing(show=False, ii=0):
         for Vg in [0, 1/4]:
             an_sol = AnalyticComovingMagnetosonicWave(k, H0, Vs, Va, Vg, gamma, ai, A_u, A_rho)
             sci_sol = ScipyComovingMagnetosonicWave(k, H0, Vs, Va, Vg, gamma, ai, A_u, A_rho)
-
-            if show:
-                import matplotlib.pyplot as plt
-                plt.figure(ii+1)
-                plt.clf()
-                fig, axes = plt.subplots(num=ii+1, nrows=2, ncols=2, sharex=True)
-
-                axes[0, 0].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).real, label='Analytic')
-                axes[0, 1].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).imag)
-                axes[1, 0].semilogx(a, an_sol.delta_u_over_vs(a).real)
-                axes[1, 1].semilogx(a, an_sol.delta_u_over_vs(a).imag)
-                axes[0, 0].set_title(r'$\mathrm{Re}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
-                axes[0, 1].set_title(r'$\mathrm{Im}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
-                axes[1, 0].set_title(r'$\mathrm{Re}(\delta u/\mathcal{V}_\mathrm{A})$')
-                axes[1, 1].set_title(r'$\mathrm{Im}(\delta u/\mathcal{V}_\mathrm{A})$')
-
-                axes[0, 0].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).real, '--', label='Scipy')
-                axes[0, 1].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).imag, '--')
-                axes[1, 0].semilogx(a, sci_sol.delta_u_over_vs(a).real, '--')
-                axes[1, 1].semilogx(a, sci_sol.delta_u_over_vs(a).imag, '--')
-                axes[0, 0].legend(frameon=False)
-                nums = '{:1.2f},{:1.2f},{:1.2f}'.format(an_sol.OmegaS/np.pi, an_sol.OmegaA/np.pi, an_sol.OmegaG/np.pi)
-                title = r'$\gamma=4/3,\, (\Omega_\mathrm{s},\,\Omega_\mathrm{A},\,\Omega_\mathrm{g})/\pi = ' + nums + '$'
-                fig.suptitle(title)
-                ii += 1
-
-                # Test that everything works
-                np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).real,
-                                           sci_sol.delta_rhoc_over_rhoc(a).real, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).imag,
-                                           sci_sol.delta_rhoc_over_rhoc(a).imag, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_u_over_vs(a).real,
-                                           sci_sol.delta_u_over_vs(a).real, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_u_over_vs(a).imag,
-                                           sci_sol.delta_u_over_vs(a).imag, atol=1e-4)
+            compare(show, a, an_sol, sci_sol)
 
     Vs = 1/4 * H0/k
+    Va = 0
+    Vg = 0
 
     an_sol = AnalyticComovingMagnetosonicWave(k, H0, Vs, Va, Vg, gamma, ai, A_u, A_rho)
     sci_sol = ScipyComovingMagnetosonicWave(k, H0, Vs, Va, Vg, gamma, ai, A_u, A_rho)
-
-    if show:
-        import matplotlib.pyplot as plt
-        plt.figure(ii+1)
-        plt.clf()
-        fig, axes = plt.subplots(num=ii+1, nrows=2, ncols=2, sharex=True)
-
-        axes[0, 0].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).real, label='Analytic')
-        axes[0, 1].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).imag)
-        axes[1, 0].semilogx(a, an_sol.delta_u_over_vs(a).real)
-        axes[1, 1].semilogx(a, an_sol.delta_u_over_vs(a).imag)
-        axes[0, 0].set_title(r'$\mathrm{Re}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
-        axes[0, 1].set_title(r'$\mathrm{Im}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
-        axes[1, 0].set_title(r'$\mathrm{Re}(\delta u/\mathcal{V}_\mathrm{A})$')
-        axes[1, 1].set_title(r'$\mathrm{Im}(\delta u/\mathcal{V}_\mathrm{A})$')
-
-        axes[0, 0].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).real, '--', label='Scipy')
-        axes[0, 1].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).imag, '--')
-        axes[1, 0].semilogx(a, sci_sol.delta_u_over_vs(a).real, '--')
-        axes[1, 1].semilogx(a, sci_sol.delta_u_over_vs(a).imag, '--')
-        axes[0, 0].legend(frameon=False)
-        nums = '{:1.2f},{:1.2f},{:1.2f}'.format(an_sol.OmegaS/np.pi, an_sol.OmegaA/np.pi, an_sol.OmegaG/np.pi)
-        title = r'$\gamma=4/3,\, (\Omega_\mathrm{s},\,\Omega_\mathrm{A},\,\Omega_\mathrm{g})/\pi = ' + nums + '$'
-        fig.suptitle(title)
-        ii += 1
-
-        # Test that everything works
-        np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).real,
-                                   sci_sol.delta_rhoc_over_rhoc(a).real, atol=1e-4)
-        np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).imag,
-                                   sci_sol.delta_rhoc_over_rhoc(a).imag, atol=1e-4)
-        np.testing.assert_allclose(an_sol.delta_u_over_vs(a).real,
-                                   sci_sol.delta_u_over_vs(a).real, atol=1e-4)
-        np.testing.assert_allclose(an_sol.delta_u_over_vs(a).imag,
-                                   sci_sol.delta_u_over_vs(a).imag, atol=1e-4)
-    if show:
-        plt.show()
-
-    return ii
+    compare(show, a, an_sol, sci_sol)
 
 
 def test_gamma_five_thirds_compressible_wave_standing(show=False, ii=0):
@@ -183,45 +115,7 @@ def test_gamma_five_thirds_compressible_wave_standing(show=False, ii=0):
         for Vg in [0, 1/4]:
             an_sol = AnalyticComovingMagnetosonicWave(k, H0, Vs, Va, Vg, gamma, ai, A_u, A_rho)
             sci_sol = ScipyComovingMagnetosonicWave(k, H0, Vs, Va, Vg, gamma, ai, A_u, A_rho)
-
-            if show:
-                import matplotlib.pyplot as plt
-                plt.figure(ii+1)
-                plt.clf()
-                fig, axes = plt.subplots(num=ii+1, nrows=2, ncols=2, sharex=True)
-
-                axes[0, 0].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).real, label='Analytic')
-                axes[0, 1].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).imag)
-                axes[1, 0].semilogx(a, an_sol.delta_u_over_vs(a).real)
-                axes[1, 1].semilogx(a, an_sol.delta_u_over_vs(a).imag)
-                axes[0, 0].set_title(r'$\mathrm{Re}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
-                axes[0, 1].set_title(r'$\mathrm{Im}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
-                axes[1, 0].set_title(r'$\mathrm{Re}(\delta u/\mathcal{V}_\mathrm{A})$')
-                axes[1, 1].set_title(r'$\mathrm{Im}(\delta u/\mathcal{V}_\mathrm{A})$')
-
-                axes[0, 0].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).real, '--', label='Scipy')
-                axes[0, 1].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).imag, '--')
-                axes[1, 0].semilogx(a, sci_sol.delta_u_over_vs(a).real, '--')
-                axes[1, 1].semilogx(a, sci_sol.delta_u_over_vs(a).imag, '--')
-                axes[0, 0].legend(frameon=False)
-                nums = '{:1.2f},{:1.2f},{:1.2f}'.format(an_sol.OmegaS/np.pi, an_sol.OmegaA/np.pi, an_sol.OmegaG/np.pi)
-                title = r'$\gamma=5/3,\, (\Omega_\mathrm{s},\,\Omega_\mathrm{A},\,\Omega_\mathrm{g})/\pi = ' + nums + '$'
-                fig.suptitle(title)
-                ii += 1
-
-                # Test that everything works
-                np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).real,
-                                           sci_sol.delta_rhoc_over_rhoc(a).real, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).imag,
-                                           sci_sol.delta_rhoc_over_rhoc(a).imag, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_u_over_vs(a).real,
-                                           sci_sol.delta_u_over_vs(a).real, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_u_over_vs(a).imag,
-                                           sci_sol.delta_u_over_vs(a).imag, atol=1e-4)
-    if show:
-        plt.show()
-
-    return ii
+            compare(show, a, an_sol, sci_sol)
 
 
 def test_gamma_nonstandard_compressible_wave_standing(show=False, ii=0):
@@ -255,45 +149,7 @@ def test_gamma_nonstandard_compressible_wave_standing(show=False, ii=0):
         for Vg in [0, 1/4]:
             an_sol = AnalyticComovingMagnetosonicWave(k, H0, Vs, Va, Vg, gamma, ai, A_u, A_rho)
             sci_sol = ScipyComovingMagnetosonicWave(k, H0, Vs, Va, Vg, gamma, ai, A_u, A_rho)
-
-            if show:
-                import matplotlib.pyplot as plt
-                plt.figure(ii+1)
-                plt.clf()
-                fig, axes = plt.subplots(num=ii+1, nrows=2, ncols=2, sharex=True)
-
-                axes[0, 0].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).real, label='Analytic')
-                axes[0, 1].semilogx(a, an_sol.delta_rhoc_over_rhoc(a).imag)
-                axes[1, 0].semilogx(a, an_sol.delta_u_over_vs(a).real)
-                axes[1, 1].semilogx(a, an_sol.delta_u_over_vs(a).imag)
-                axes[0, 0].set_title(r'$\mathrm{Re}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
-                axes[0, 1].set_title(r'$\mathrm{Im}(\delta \rho_\mathrm{c}/\rho_\mathrm{c})$')
-                axes[1, 0].set_title(r'$\mathrm{Re}(\delta u/\mathcal{V}_\mathrm{A})$')
-                axes[1, 1].set_title(r'$\mathrm{Im}(\delta u/\mathcal{V}_\mathrm{A})$')
-
-                axes[0, 0].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).real, '--', label='Scipy')
-                axes[0, 1].semilogx(a, sci_sol.delta_rhoc_over_rhoc(a).imag, '--')
-                axes[1, 0].semilogx(a, sci_sol.delta_u_over_vs(a).real, '--')
-                axes[1, 1].semilogx(a, sci_sol.delta_u_over_vs(a).imag, '--')
-                axes[0, 0].legend(frameon=False)
-                nums = '{:1.2f},{:1.2f},{:1.2f}'.format(an_sol.OmegaS/np.pi, an_sol.OmegaA/np.pi, an_sol.OmegaG/np.pi)
-                title = r'$\gamma=1.2,\, (\Omega_\mathrm{s},\,\Omega_\mathrm{A},\,\Omega_\mathrm{g})/\pi = ' + nums + '$'
-                fig.suptitle(title)
-                ii += 1
-
-                # Test that everything works
-                np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).real,
-                                           sci_sol.delta_rhoc_over_rhoc(a).real, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_rhoc_over_rhoc(a).imag,
-                                           sci_sol.delta_rhoc_over_rhoc(a).imag, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_u_over_vs(a).real,
-                                           sci_sol.delta_u_over_vs(a).real, atol=1e-4)
-                np.testing.assert_allclose(an_sol.delta_u_over_vs(a).imag,
-                                           sci_sol.delta_u_over_vs(a).imag, atol=1e-4)
-    if show:
-        plt.show()
-
-    return ii
+            compare(show, a, an_sol, sci_sol)
 
 
 if __name__ == '__main__':
